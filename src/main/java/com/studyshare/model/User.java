173b5f731c11162e6928ae3509680608277534a5
Harshit1607 <hbareja.07@gmail.com>
2025-04-07 22:19:15 +0530

    backend running

--- src/main/java/com/studyshare/model/User.java
+++ src/main/java/com/studyshare/model/User.java
@@ -3,17 +3,33 @@ package com.studyshare.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+
+    private String name;
     private String email;
+
+    // ✅ Added password field
     private String password;
+
+    // If you use Lombok's @Data, the getter/setter are automatically generated.
+    // But if you'd prefer to explicitly define them:
+
+    /*
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    */
 }
